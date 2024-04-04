998370da5e307fa198a03e2d02a8f0e135db43fd
chsiao58 <chsiao58@gmail.com>
2024-04-03 21:57:47 -0400

    relocate from original fork

---
+++ ArtisanGood.cs
@@ -0,0 +1,13 @@
+﻿namespace BetterArtisanGoodIcons
+{
+    /// <summary>The artisan goods that we add unique icons foor.</summary>
+    internal enum ArtisanGood
+    {
+        Jelly = 130,
+        Pickles = 128,
+        Wine = 123,
+        Juice = 125,
+        Honey = 340         // Honey is still on springObject sheet and has a different index
+    }
+
+}
---
+++ Content/ContentPackSource.cs
@@ -0,0 +1,28 @@
+﻿using StardewModdingAPI;
+
+namespace BetterArtisanGoodIcons.Content
+{
+    /// <inheritdoc />
+    /// <summary>A content source that comes from Content Packs. </summary>
+    internal class ContentPackSource : TextureDataContentSource
+    {
+        private readonly IContentPack pack;
+        public override CustomTextureData TextureData { get; }
+
+        public ContentPackSource(IContentPack pack)
+        {
+            this.pack = pack;
+            this.TextureData = pack.ReadJsonFile<CustomTextureData>("data.json");
+        }
+
+        public override T Load<T>(string path)
+        {
+            return this.pack.ModContent.Load<T>(path);
+        }
+
+        public override IManifest GetManifest()
+        {
+            return this.pack.Manifest;
+        }
+    }
+}
